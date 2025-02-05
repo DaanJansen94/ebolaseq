5ca0113f8ea251c1f357392d0672adeebf478002
DaanJansen94 <jansendaan94@gmail.com>
2025-02-05 16:56:21 +0100

    .

--- ebolaseq/cli.py
+++ ebolaseq/cli.py
@@ -3,12 +3,22 @@ from ebolaseq.core import main
 
 def cli_main():
     """Entry point for the command line interface."""
-    parser = argparse.ArgumentParser(description='Process Ebola virus sequences.')
+    parser = argparse.ArgumentParser(
+        description='Download and process Ebola virus sequences from GenBank.',
+        usage='%(prog)s -output_loc <output_directory>'
+    )
+    
     parser.add_argument('-output_loc', '--output_location', 
                         type=str,
                         required=True,
                         help='Output directory for results (use "." for current directory)')
     
+    # If no arguments are provided, print help and exit
+    import sys
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+        
     args = parser.parse_args()
     
     # Create output directory if it doesn't exist
