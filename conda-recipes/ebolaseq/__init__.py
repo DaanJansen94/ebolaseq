dff1dfa03ea65ba444ff818f64539446fb227f2c
DaanJansen94 <jansendaan94@gmail.com>
2025-07-10 10:36:59 +0200

    .

--- conda-recipes/ebolaseq/__init__.py
+++
@@ -1,2 +0,0 @@
-# Simple hardcoded version
-__version__ = "v0.1.4" 
--- setup.py
+++ setup.py
@@ -2,18 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="ebolaseq",
-    use_scm_version={
-        "version_scheme": "post-release",
-        "local_scheme": "dirty-tag"
-    },
-    setup_requires=["setuptools_scm"],
+    version="0.1.4",
     description="A tool for filtering and analyzing Ebola virus sequences",
     author="Daan Jansen",
     packages=find_packages(),
     python_requires=">=3.9",
     install_requires=[
         "biopython>=1.81",
-        "setuptools_scm",
     ],
     entry_points={
         'console_scripts': [
