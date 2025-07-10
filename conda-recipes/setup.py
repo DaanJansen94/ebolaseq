2235e83e615077a61bf257b2d118397e3122e054
DaanJansen94 <jansendaan94@gmail.com>
2025-07-10 10:27:33 +0200

    Simplify version to hardcoded v0.1.4

--- conda-recipes/setup.py
+++ conda-recipes/setup.py
@@ -1,16 +1,8 @@
 from setuptools import setup, find_packages
 
-# Try to get version from setuptools_scm, fallback to hardcoded version
-try:
-    from setuptools_scm import get_version
-    version = get_version()
-except (ImportError, LookupError):
-    # Fallback version when setuptools_scm fails (e.g., in conda-build)
-    version = "0.1.5"
-
 setup(
     name="ebolaseq",
-    version=version,
+    version="0.1.4",
     description="A tool for filtering and analyzing Ebola virus sequences",
     author="Daan Jansen",
     packages=find_packages(),
