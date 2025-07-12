226c7a505e4d318fc1310e02faae87879247ad1a
DaanJansen94 <jansendaan94@gmail.com>
2025-07-12 21:06:43 +0200

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ebolaseq",
-    version="0.1.4",
+    version="0.1.5",
     description="A tool for filtering and analyzing Ebola virus sequences",
     author="Daan Jansen",
     packages=find_packages(),
